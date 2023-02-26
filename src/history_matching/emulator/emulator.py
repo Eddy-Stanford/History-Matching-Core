36ad0f9bb7561cfe2b11c685b34d7b2a1d3dc900
Robert King <robertkng104@gmail.com>
2023-02-25 21:39:48 -0800

    added predicting from sample space

--- src/history_matching/emulator/emulator.py
+++ src/history_matching/emulator/emulator.py
@@ -1,7 +1,10 @@
 import numpy as np
+import xarray as xr
 from sklearn.base import BaseEstimator
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.preprocessing import StandardScaler
+
+from ..samples import SampleSpace
 
 
 def implausibility(
@@ -36,6 +39,10 @@ class GPEmulator(BaseEstimator):
         self.__std_y = None
 
     def fit(self, X, y, y_err):
+        if len(y.shape) == 1:
+            y = y[:, None]
+        if len(y_err.shape) == 1:
+            y_err = y_err[:, None]
         self.__mean_y = np.mean(y, axis=0)
         self.__std_y = np.std(y, axis=0)
         y = (y - self.__mean_y) / self.__std_y
@@ -59,7 +66,49 @@ class GPEmulator(BaseEstimator):
         X = self.scaler.transform(X)
         values = np.array([gp.predict(X, return_std=return_std) for gp in self.gps])
         if return_std:
-            return (values[:, 0, :] * self.__std_y) + self.__mean_y, values[
-                :, 1, :
-            ] * self.__std_y
-        return values * self.__std_y + self.__mean_y
+            return np.transpose(
+                (values[:, 0, :] * self.__std_y) + self.__mean_y
+            ), np.transpose(values[:, 1, :] * self.__std_y)
+        return np.transpose(values * self.__std_y + self.__mean_y)
+
+    def predict_over_space(self, space: SampleSpace, return_std=False, resolution=None):
+        """
+        Run prediction over sample space
+        """
+        space_up = space.to_xarray(resolution=resolution)
+        valid_points = np.array(np.nonzero(space_up.values))
+        X = np.zeros(valid_points.shape)
+        for i, dim in enumerate(space_up.dims):
+            X[i, :] = space_up[dim][valid_points[i, :]]
+        X = np.transpose(X)
+
+        # TODO: This can almost certainly be implemented better with less code duplication
+        if return_std:
+            pred, pred_std = self.predict(X, return_std=True)
+            predictions = np.empty((self.n_features, *space_up.shape)) * np.nan
+            predictions_std = np.empty((self.n_features, *space_up.shape)) * np.nan
+            for pred_point, pred_std, valid_point in zip(
+                pred, pred_std, valid_points.T
+            ):
+                predictions[(slice(None), *valid_point)] = pred_point
+                predictions_std[(slice(None), *valid_point)] = pred_std
+            return xr.DataArray(
+                predictions,
+                dims=("n_features", *space_up.dims),
+                coords={"n_features": np.arange(self.n_features), **space_up.coords},
+            ), xr.DataArray(
+                predictions_std,
+                dims=("n_features", *space_up.dims),
+                coords={"n_features": np.arange(self.n_features), **space_up.coords},
+            )
+
+        else:
+            pred = self.predict(X, return_std=False)
+            predictions = np.empty((self.n_features, *space_up.shape)) * np.nan
+            for pred_point, valid_point in zip(pred, valid_points.T):
+                predictions[(slice(None), *valid_point)] = pred_point
+            return xr.DataArray(
+                predictions,
+                dims=("n_features", *space_up.dims),
+                coords={"n_features": np.arange(self.n_features), **space_up.coords},
+            )
