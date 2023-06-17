266948d9c4335ed16b1fa872514b8cc8bf49e27d
Robert King <robcking@stanford.edu>
2023-06-17 16:26:04 -0700

    i wish this to work

--- src/history_matching/emulator/emulator.py
+++ src/history_matching/emulator/emulator.py
@@ -34,7 +34,7 @@ class GPEmulator(BaseEstimator):
         else:
             self.__mean_y = y[0]
             self.__std_y = np.ones(y[0].shape)
-            y = np.zeros(y[0].shape)
+            y = np.zeros(y.shape)
 
         self.scaler.fit(X)
         X = self.scaler.transform(X)
